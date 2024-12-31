c6bb207650b0d03e7dc72ac1ad5fb9817abceb9d
emilhsy <emilhsy@gmail.com>
2024-12-31 18:46:11 +0100

    Third Task FigmaLayout

---
+++ app/src/main/java/com/example/mobileapp/CreateAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobileapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account2)
+    }
+}
--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.mobileapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val createAccountButton: Button = findViewById(R.id.createAccountButton)
+        val signInButton: Button = findViewById(R.id.signInButton)
+
+        createAccountButton.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        signInButton.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
