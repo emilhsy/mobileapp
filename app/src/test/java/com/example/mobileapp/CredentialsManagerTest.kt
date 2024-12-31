b05b4200b316ab57d1642b622228fd01b497234e
emilhsy <emilhsy@gmail.com>
2024-12-31 18:11:24 +0100

    Initial Commit

---
+++ app/src/test/java/com/example/mobileapp/CredentialsManagerTest.kt
@@ -0,0 +1,40 @@
+package com.example.learning
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+
+class CredentialsManagerTest {
+
+    private val credentialsManager = CredentialsManager()
+
+    @Test
+    fun givenEmptyEmail_thenReturnFalse() {
+        val isEmailValid = credentialsManager.isEmailValid("")
+        assertEquals(false, isEmailValid)
+    }
+
+    @Test
+    fun givenWrongEmailFormat_thenReturnFalse() {
+        val isEmailValid = credentialsManager.isEmailValid("invalidEmail.com")
+        assertEquals(false, isEmailValid)
+    }
+
+    @Test
+    fun givenProperEmailFormat_thenReturnTrue() {
+        val isEmailValid = credentialsManager.isEmailValid("user@example.com")
+        assertEquals(true, isEmailValid)
+    }
+
+    @Test
+    fun givenEmptyPassword_thenReturnFalse() {
+        val isPasswordValid = credentialsManager.isPasswordValid("")
+        assertEquals(false, isPasswordValid)
+    }
+
+    @Test
+    fun givenFilledPassword_thenReturnTrue() {
+        val isPasswordValid = credentialsManager.isPasswordValid("mypassword123")
+        assertEquals(true, isPasswordValid)
+    }
+}
